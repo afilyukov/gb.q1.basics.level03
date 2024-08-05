94674bb909cbc578355c0c75c8f4647ed2d54fd7
veles <filyukvo@yandex.ru>
2024-08-05 22:32:54 +0300

    initial commit

---
+++ Module01/src/Module01.java
@@ -0,0 +1,6 @@
+public class Module01 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ Module03/src/Module03.java
@@ -0,0 +1,6 @@
+public class Module03
+{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
